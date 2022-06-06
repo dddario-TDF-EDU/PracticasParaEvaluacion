87c1120b10c4d6e9f38627dc0263aaa94d797769
dddario-TDF-EDU <103612977+dddario-TDF-EDU@users.noreply.github.com>
2022-06-05 23:16:15 -0300

    Terminado ejercicio del Cine

--- ejerciciosProg/personasEnElCine.ts
+++ ejerciciosProg/personasEnElCine.ts
@@ -9,13 +9,18 @@ function verificacionNumero(numLocal: number): boolean {
 }
 
 function asignarBoolean(): boolean {
-  let valor: number = math.floor(math.random() * (1 - 0) + 0);
-  return valor;
+  let valorBoolean: boolean;
+  let valor: number = Math.floor(Math.random() * (2 - 0) + 0);
+  if (valor === 1) {
+    valorBoolean = true;
+  } else {
+    valorBoolean = false;
+  }
+  return valorBoolean;
 }
 
 function cargaButacas(arrayButaca: boolean[]) {
   let longitudArray: number = arrayButaca.length - 1;
-
   for (let elemento: number = 0; elemento <= longitudArray; elemento++) {
     arrayButaca[elemento] = asignarBoolean();
   }
@@ -36,17 +41,14 @@ function contarButacas(arrayButaca: boolean[]) {
   console.log("La cantidad de butacas desocupadas es: " + butacaDesocupada);
 }
 
-botonaso?.addEventListener("click", () => {
+botonaso.addEventListener("click", () => {
   let numeroValido: boolean;
-  let tamanioArray: number;
-
   numeroValido = verificacionNumero(Number(numIngresado.value));
-
   if (numeroValido === true) {
-    tamanioArray = Number(numIngresado.value);
-    let cantidadButacas: boolean = new Array(tamanioArray);
+    let cantidadButacas: boolean = new Array(Number(numIngresado.value));
     cargaButacas(cantidadButacas);
     contarButacas(cantidadButacas);
+    console.log("La cantidad de butacas es: " + cantidadButacas.length);
   } else {
     console.log("El dato ingresado no es correcto, intentelo nuevamente");
   }
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 let botonaso = document.getElementById("Ir");
 
-botonaso?.addEventListener("click", () => {
-  let direccion: number = Number(eleccion.value);
+botonaso.addEventListener("click", () => {
+  let direccion: number = Number(pasito.value);
 
   switch (direccion) {
     case 1:
       location.href = "../ejerciciosProg/personasEnElCine.html";
       break;
+    default:
+      console.log("que onda we no carga");
   }
 });
